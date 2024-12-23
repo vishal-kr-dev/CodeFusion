9ea2344796c93021c0196cf710005f2bf3cba98f
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2024-12-23 10:39:35 +0530

    Changes

--- client/src/atoms.jsx
+++ client/src/atoms.jsx
@@ -1,33 +1,31 @@
-import {atom} from "recoil";
+import { atom } from "recoil";
 
 // saving previously selected language and theme to local storage
-const localStorageEffect = key => ({setSelf, onSet}) => {
-    const savedValue = localStorage.getItem(key)
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
     if (savedValue != null) {
-        setSelf(JSON.parse(savedValue));
+      setSelf(JSON.parse(savedValue));
     }
 
-    onSet(newValue => {
-        //   if (newValue instanceof DefaultValue) {
-        //     localStorage.removeItem(key);
-        //   } else {
-        localStorage.setItem(key, JSON.stringify(newValue));
-        //   }
+    onSet((newValue) => {
+      //   if (newValue instanceof DefaultValue) {
+      //     localStorage.removeItem(key);
+      //   } else {
+      localStorage.setItem(key, JSON.stringify(newValue));
+      //   }
     });
-};
+  };
 
 export const language = atom({
-    key: "language",
-    default: "javascript",
-    effects_UNSTABLE: [
-        localStorageEffect('language'),
-    ]
+  key: "language",
+  default: { id: "63", value: "javascript" },
+  effects_UNSTABLE: [localStorageEffect("language")],
 });
 
 export const cmtheme = atom({
-    key: "cmtheme",
-    default: "monokai",
-    effects_UNSTABLE: [
-        localStorageEffect('cmtheme'),
-    ]
+  key: "cmtheme",
+  default: "monokai",
+  effects_UNSTABLE: [localStorageEffect("cmtheme")],
 });
--- client/src/components/OutputWindow.jsx
+++ client/src/components/OutputWindow.jsx
@@ -35,10 +35,8 @@ const OutputWindow = ({ outputDetails }) => {
   };
   return (
     <>
-      <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
-        Output
-      </h1>
-      <div className="w-full h-56 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
+      <h1 className="font-bold text-2xl text-white flex justify-center bg-[#1e293b] border-b">Output</h1>
+      <div className="w-full h-56 bg-[#1e293b] text-white font-normal text-xl overflow-y-auto">
         {outputDetails ? <>{getOutput()}</> : null}
       </div>
     </>
