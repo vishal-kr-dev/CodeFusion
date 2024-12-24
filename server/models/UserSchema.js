dcceaa77a263a46378d9bd27bc09248fee7eb065
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2024-12-24 11:57:52 +0530

    Updated UserSchema for records addition

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
+const recordSchema = new mongoose.Schema({
+  roomId: { type: String, required: true },
+  data: { type: String, required: true },
+});
+
 const UserSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  roomId: {type: String, unique: true}
+  roomId: {type: String, unique: true},
+  records: [recordSchema]
 });
 
 const UserModel = mongoose.model("User", UserSchema);
